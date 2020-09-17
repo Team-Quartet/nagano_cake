f9dca9ef5068d6ceda7ebab46c389fc7c4826427
kazarimono3 <t.kaneda@KANEDA-PCnoMacBook-Pro.local>
2020-09-17 23:51:33 +0900

    top・aboutページ追加 / shipping_addressバリデーション追加

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,4 +1,11 @@
 class ShippingAddress < ApplicationRecord
-
-belongs_to :customer
+	
+	validates :last_name,  presence: true
+	validates :first_name, presence: true
+	
+	# 郵便番号 ７桁固定
+	validates :post_code,  presence: true, format: {with: /\A\d{7}\z/}
+	validates :address, presence: true
+   
+	belongs_to :customer
 end
