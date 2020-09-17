244076ddcc93acfc3bac82fe116cdb59caa4bbb8
KOJI-YAMABE <kvbl.ak25@gmail.com>
2020-09-17 23:05:03 +0900

    カスタマーのログイン機能実装

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,11 +2,18 @@ class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
 
- has_many :cart_items
- has_many :items, through: :cart_items
- has_many :orders
- has_many :shipping_addresses
+  has_many :cart_items
+  has_many :items, through: :cart_items
+  has_many :orders
+  has_many :shipping_addresses
 
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :kana_family_name, presence: true
+  validates :kana_first_name, presence: true
+  validates :postal_code, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
 end
