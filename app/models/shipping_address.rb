d4a9ed46e8adfe3497193679e9dc1ec176c3a332
kazarimono3 <t.kaneda@KANEDA-PCnoMacBook-Pro.local>
2020-09-18 00:06:18 +0900

    バリデーション修正

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,11 +1,12 @@
 class ShippingAddress < ApplicationRecord
 	
-	validates :last_name,  presence: true
+	validates :family_name,  presence: true
 	validates :first_name, presence: true
 	
 	# 郵便番号 ７桁固定
-	validates :post_code,  presence: true, format: {with: /\A\d{7}\z/}
+	validates :postal_code,  presence: true, format: {with: /\A\d{7}\z/}
 	validates :address, presence: true
+	validates :receiver_name, presence: true
    
 	belongs_to :customer
 end
