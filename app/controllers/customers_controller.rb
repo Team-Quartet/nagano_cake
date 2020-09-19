0b48736321b68346e6146f2a1619881b9538750d
kazarimono3 <t.kaneda@KANEDA-PCnoMacBook-Pro.local>
2020-09-20 03:29:36 +0900

    マイページ・登録情報編集・退会ページの追加

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,16 +1,39 @@
 class CustomersController < ApplicationController
+  before_action :authenticate_customer!
+
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to customer_path(current_customer.id), notice: "登録情報を編集しました"
+    else
+      render :edit
+    end
   end
 
   def unsubscribe
+    @customer = current_customer
   end
 
   def withdraw
+    @customer = current_customer
+    @customer.destroy
+    redirect_to root_path, notice: "退会しました"
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:family_name, :first_name, :kana_family_name, :kana_first_name, :email, :postal_code, :address, :phone_number)
   end
 end
