d739c16b4fe68c933fae4989f7a1f8114f021cd7
vagrant <vagrant@localhost.localdomain>
2020-09-19 04:58:00 +0000

    商品ジャンル機能追加

--- db/migrate/20200917020111_create_items.rb
+++ db/migrate/20200917020111_create_items.rb
@@ -6,7 +6,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.text :introduction
       t.integer :non_taxed_price
       t.string :image_id
-      t.boolean :is_active
+      t.boolean :is_active, default: false, null: false
 
       t.timestamps
     end
