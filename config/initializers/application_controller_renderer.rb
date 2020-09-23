abe5f74df0d6466a5c77742f920d5940995a5cb1
KOJI-YAMABE <kvbl.ak25@gmail.com>
2020-09-23 11:56:35 +0900

    topページ編集

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'abb7fadbcab185b4d12e991a584d7acb2a281923ad88de7fe9f9c5f94cc2b9abb658414e2caf4e5d30e9694b92e19ff311622e71fdc6c7ce3c83f7eccf45181a'
