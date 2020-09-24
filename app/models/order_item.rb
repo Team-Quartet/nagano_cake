417308b5f9fbbcbbafbe4b9a0392f13607f628db
KOJI-YAMABE <kvbl.ak25@gmail.com>
2020-09-24 10:29:50 +0900

    注文履歴一覧詳細画面編集しました

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,5 +1,16 @@
 class OrderItem < ApplicationRecord
 
-belongs_to :order
-belongs_to :item
+	belongs_to :order
+	belongs_to :item
+
+	enum making_status: [:着手不可, :製作待ち, :製作中, :製作完了]
+
+	after_update do
+		order_items = self.order.order_items
+		if order_items.any? {|order_item| order_item.making_status == "製作中"} == true #製作ステータスが1つでも製作中になったら
+		    self.order.update(status: "製作中") # 注文ステータスを製作中に変更する
+		else order_items.all? {|order_item| order_item.making_status == "製作完了"} == true #製作ステータスが全て製作完了だったら
+		    self.order.update(status: "発送準備中") # 注文ステータスを発送準備中に変更する
+		end
+	end
 end
